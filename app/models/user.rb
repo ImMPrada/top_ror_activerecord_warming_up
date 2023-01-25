901021ae878c9b060ef6bfef9ff48566de9af02c
immprada <im.mprada@gmail.com>
2023-01-25 13:53:54 -0500

    update Country and User models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_one :profile
+  has_one :profile, dependent: :destroy
 
   validates :email, presence: true, uniqueness: true
   validates :birthdate, presence: true
