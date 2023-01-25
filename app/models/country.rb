0211cc6934fea0d5da8d430ab17666fea315ed97
immprada <im.mprada@gmail.com>
2023-01-25 13:42:34 -0500

    add Country model

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,6 +1,6 @@
 class Country < ApplicationRecord
   has_many :states
-  has_many :cities, trough: states
+  has_many :cities, through: :states
 
   validates :name, presence: true, uniqueness: true
 end
